2c16a52004b1d35516fe7f4b316e5cbf7c15675e
Lexa74 <you@example.com>
2022-08-25 15:53:40 +0500

    костыльно сделал табы)

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let tab = 0;
-/*document.addEventListener('click', function () {
-    alert('Вы кликнули по странице!')
-  });*/
-document.getElementsByClassName('.tab1').onClick = function () {
-    alert('URA');
-    this.classList.add('active');
-}
+let tab = document.querySelectorAll('.tabs > div');
+let tabContent = document.querySelectorAll('.contentTabs > div');
 
+
+tab[0].addEventListener('click', function () {
+  tab[0].classList.remove('active');
+  tab[1].classList.remove('active');
+  tabContent[0].classList.remove('active');
+  tabContent[1].classList.remove('active');
+  tab[0].classList.add('active');
+  tabContent[0].classList.add('active');
+ });
+
+ tab[1].addEventListener('click', function () {
+  tab[0].classList.remove('active');
+  tab[1].classList.remove('active');
+  tabContent[0].classList.remove('active');
+  tabContent[1].classList.remove('active');
+  tab[1].classList.add('active');
+  tabContent[1].classList.add('active');
+ });
+
+ /*let tab = document.querySelectorAll('.tabChange');
+ alert(tab[0]);
+ tab[0].addEventListener('click', function () {
+    alert(tab);
+ });*/
