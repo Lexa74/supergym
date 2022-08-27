4d3a86a735564a16fec5c4c05d411fda552f2215
Lexa74 <you@example.com>
2022-08-27 15:23:39 +0500

    html не залился

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let tab = document.querySelectorAll('.tabs > div');
-let tabContent = document.querySelectorAll('.contentTabs > div');
+let tabs = document.querySelectorAll('.tabs > div');
+let tabsContent = document.querySelectorAll('.contentTabs > div');
 let lengthTabs = document.getElementById('forLength').children.length;
 
 /*tab[0].addEventListener('click', function () {
@@ -26,33 +26,47 @@ let lengthTabs = document.getElementById('forLength').children.length;
     alert(tab);
  });*/
 
- let numberTab = 0;
+//  let numberTab = 0;
 
- function tabChange(numberTab) {
-  let i = 0;
-  while (i < lengthTabs) {
-    tab[i].classList.remove('active');
-    tabContent[i].classList.remove('active');
-    i++;
-  }
-  tab[numberTab].classList.add('active');
-  tabContent[numberTab].classList.add('active');
- }
+//  function tabChange(numberTab) {
+//   let i = 0;
+//   while (i < lengthTabs) {
+//     tabs[i].classList.remove('active');
+//     tabsContent[i].classList.remove('active');
+//     i++;
+//   }
+//   tabs[numberTab].classList.add('active');
+//   tabsContent[numberTab].classList.add('active');
+//  }
 
- function clickHandler(elem,elems) {
+ function clickHandler(elem,elems,clickCallback = null) {
   elem.addEventListener('click', function(event) {
     event.preventDefault();
     resetElementes(elems);
     elem.classList.add('active');
+    if (clickCallback != null) {
+      clickCallback();
+    } 
   })
  };
+
 
 function resetElementes(elems) {
   elems.forEach(function(elem) {elem.classList.remove('active')});
 } 
 
+function changeTabContent(tabContent, tabsContent) {
+  resetElementes(tabsContent);
+  tabContent.classList.add('active');
+}
+
  let buttons = document.querySelectorAll('.rank > button');
  buttons.forEach(function(button) {
   clickHandler(button,buttons);
+ }) 
+
+ tabs.forEach(function(tab,index) {
+  let changeTab = function(){changeTabContent(tabsContent[index], tabsContent)};
+  clickHandler(tab,tabs, changeTab);
  }) 
 
