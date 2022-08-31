7445137db3ba40a08c773ceff3c08fffbbbdee60
Lexa74 <you@example.com>
2022-08-31 15:42:40 +0500

    Сделал попап

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let tabs = document.querySelectorAll('.tabs > div');
 let tabsContent = document.querySelectorAll('.contentTabs > div');
-let lengthTabs = document.getElementById('forLength').children.length;
+let popUp = document.getElementById('popUp');
+let closePopUp = document.querySelectorAll('.changePopUp');
+
 
 /*tab[0].addEventListener('click', function () {
   tab[0].classList.remove('active');
@@ -39,8 +41,8 @@ let lengthTabs = document.getElementById('forLength').children.length;
 //   tabsContent[numberTab].classList.add('active');
 //  }
 
- function clickHandler(elem,elems,clickCallback = null) {
-  elem.addEventListener('click', function(event) {
+ function clickHandler(elem,elems,clickCallback = null) {  // если нет табов, то функция работает как просто добавлению this класса active
+  elem.addEventListener('click', function(event) { // event это замена this
     event.preventDefault();
     resetElementes(elems);
     elem.classList.add('active');
@@ -50,23 +52,28 @@ let lengthTabs = document.getElementById('forLength').children.length;
   })
  };
 
-
 function resetElementes(elems) {
   elems.forEach(function(elem) {elem.classList.remove('active')});
 } 
 
-function changeTabContent(tabContent, tabsContent) {
+function changeTabContent(tabContent, tabsContent) { // Выбор содержимого таба, но мы не знаем индекс нажатого таба, поэтому есть changeTab
   resetElementes(tabsContent);
   tabContent.classList.add('active');
 }
 
  let buttons = document.querySelectorAll('.rank > button');
  buttons.forEach(function(button) {
-  clickHandler(button,buttons);
+  clickHandler(button,buttons); // функция возвращает buttons, но это используется 
  }) 
 
  tabs.forEach(function(tab,index) {
-  let changeTab = function(){changeTabContent(tabsContent[index], tabsContent)};
+  let changeTab = function(){changeTabContent(tabsContent[index], tabsContent)}; // функция возвращает индекс нужного таба
   clickHandler(tab,tabs, changeTab);
  }) 
+
+closePopUp.forEach(function(elemClose) {
+  elemClose.addEventListener('click', function() {
+    popUp.classList.toggle('active');
+  })
+ });
 
